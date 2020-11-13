b897d618f72648e1889b24d9669d145962b03147
ckomodo <chrissakwa@gmail.com>
2020-11-12 23:28:47 -0800

    pages,routes and components set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Saved from "./components/pages/saved";
+import Searched from "./components/pages/searched";
+import Background from "./components/Background";
 
 function App() {
   return (
     <div>
-      
+      <Router>
+        <Switch>
+          {/* <Background> */}
+            <Route exact path="/saved">
+              <Saved />
+            </Route>
+            <Route exact path="/searched">
+              <Searched />
+            </Route>
+            <Route exact path="/">
+              <Saved />
+            </Route>
+          {/* </Background> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/pages/searched/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Searched() {
+    return (
+        <div>
+            <p>this is the searched route</p>
+        </div>
+    )
+}
